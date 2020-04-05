82d7432b2251e58b61f3f5688ada95eb51d8e7e2
Dmitriy Dulkin <37985913+AmazingSuka@users.noreply.github.com>
2020-04-05 23:50:53 +0400

    Initial commit

---
+++ Two.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Two.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
