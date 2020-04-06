da4f414566e73d2d5762c617721457c4707261d7
Dmitriy Dulkin <37985913+AmazingSuka@users.noreply.github.com>
2020-04-06 10:02:06 +0400

    Update Startup.cs

--- Two.WebUI/Startup.cs
+++ Two.WebUI/Startup.cs
@@ -63,8 +63,6 @@ namespace Boxters.WebUI
             {
                 app.UseDeveloperExceptionPage();
                 // app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
             app.UseStaticFiles();
